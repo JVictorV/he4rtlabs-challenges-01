20c8de04920a67a7a85a857e622383d47ef00774
JVictorV <joaovictorvisona@gmail.com>
2020-03-06 08:13:03 -0300

    Some initial CSS

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('Ola He4rt :)');
+const form = document.getElementById('form');
+
+const totalTimeInput = document.getElementById('totalTime');
+const workDaysInput = document.getElementById('workDays');
+const vacancyDaysInput = document.getElementById('vacancyDays');
+const budgetInput = document.getElementById('budget');
+
