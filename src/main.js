0f58f8b367903d6d757c2d961b23955afffc04e7
João Victor <joaovictorvisona@gmail.com>
2020-03-07 12:42:20 -0300

    Implemented price calculation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,35 @@
-const form = document.getElementById('form');
-
-const totalTimeInput = document.getElementById('totalTime');
+const hoursPerDayInput = document.getElementById('hoursPerDay');
 const workDaysInput = document.getElementById('workDays');
 const vacancyDaysInput = document.getElementById('vacancyDays');
 const budgetInput = document.getElementById('budget');
 
+const resultElement = document.getElementById('result');
+
+const submitButton = document.getElementById('submit');
+
+function isFormValid() {
+    return (
+        hoursPerDayInput.checkValidity() &&
+        workDaysInput.checkValidity() &&
+        vacancyDaysInput.checkValidity() &&
+        budgetInput.checkValidity()
+    );
+}
+
+function calculateValuePerHour() {
+    const budget = Number(budgetInput.value);
+    const workDays = Number(workDaysInput.value);
+    const hoursPerDay = Number(hoursPerDayInput.value);
+    const vacancyDays = Number(vacancyDaysInput.value);
+
+    return (
+        budget / (workDays * 4 * hoursPerDay) +
+        vacancyDays * workDays * hoursPerDay
+    ).toFixed(0);
+}
+
+submitButton.onclick = () => {
+    if (isFormValid()) {
+        resultElement.innerText = `R$${calculateValuePerHour()}/h`;
+    }
+};
